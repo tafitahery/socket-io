409c2ad9c60dd72a4a4517f2d7bab7fa0894460d
Tafita Hery <tafitahery@gmail.com>
2022-12-04 17:23:02 +0300

    Navbar design with Notification

--- client/src/component/navbar/Navbar.jsx
+++ client/src/component/navbar/Navbar.jsx
@@ -1,5 +1,26 @@
 import './navbar.css';
+import Notification from '../../img/notification.svg';
+import Message from '../../img/message.svg';
+import Settings from '../../img/settings.svg';
 
 export default function Navbar() {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      <span className="logo">Lebo App</span>
+      <div className="icons">
+        <div className="icon">
+          <img src={Notification} alt="" className="iconImg" />
+          <div className="counter">2</div>
+        </div>
+        <div className="icon">
+          <img src={Message} alt="" className="iconImg" />
+          <div className="counter">2</div>
+        </div>
+        <div className="icon">
+          <img src={Settings} alt="" className="iconImg" />
+          <div className="counter">2</div>
+        </div>
+      </div>
+    </div>
+  );
 }
